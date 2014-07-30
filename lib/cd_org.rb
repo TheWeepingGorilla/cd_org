23904a42632d9b026dcaf2fc7015eebe15f9bfc0
Sean Morrison & Eric Johnson <student@epicodus.com>
2014-07-30 10:07:28 -0700

    add CD.search method

--- lib/cd_org.rb
+++ lib/cd_org.rb
@@ -16,8 +16,20 @@ class CD
     @@collection = []
   end
 
+  def CD.search arg
+    found_cd = nil
+    @@collection.each do |item|
+      if ((item.title == arg) || (item.artist == arg))
+        found_cd = item
+      end
+    end
+    found_cd
+  end
+
+
   def save
     @@collection << self
     self
   end
+
 end
--- spec/cd_org_spec.rb
+++ spec/cd_org_spec.rb
@@ -8,6 +8,7 @@ describe 'CD' do
     test_cd = CD.new({:title=> "Waiting for the Sun", :artist=> "The Doors"})
     expect(test_cd).to be_an_instance_of CD
   end
+
   describe 'CD.all' do
     it 'list all cds in the collection' do
       CD.clear
@@ -21,5 +22,17 @@ describe 'CD' do
     end
   end
 
+  describe 'CD.search' do
+    it 'allows search by title' do
+      CD.clear
+      test_cd = CD.new({:title=> "Waiting for the Sun", :artist=> "The Doors"})
+      test_cd.save
+      test_cd_two = CD.new({:title=> "Swansong", :artist=> "Carcass"})
+      test_cd_two.save
+      test_cd_three = CD.new({:title=> "Abbey Road", :artist=> "The Beatles"})
+      test_cd_three.save
+      expect(CD.search("Swansong")).to eq test_cd_two
+    end
+  end
 
 end
