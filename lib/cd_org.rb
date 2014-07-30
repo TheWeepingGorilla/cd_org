6d29420db4ad32f4e8b9aa9bc890f7595bff361e
Sean Morrison & Eric Johnson <student@epicodus.com>
2014-07-30 09:47:21 -0700

    Listing all cds and clearing cd list works.

--- lib/cd_org.rb
+++ lib/cd_org.rb
@@ -1,9 +1,23 @@
 class CD
 
   attr_reader :title, :artist
+  @@collection = []
 
   def initialize attributes
     @title = attributes[:title]
     @artist = attributes[:artist]
+  end
+
+  def CD.all
+    @@collection
+  end
+
+  def CD.clear
+    @@collection = []
+  end
+
+  def save
+    @@collection << self
+    self
   end
 end
--- spec/cd_org_spec.rb
+++ spec/cd_org_spec.rb
@@ -1,9 +1,25 @@
 require 'rspec'
 require 'cd_org'
+require 'pry'
 
 describe 'CD' do
   it 'is initialized with a hash' do
+    CD.clear
     test_cd = CD.new({:title=> "Waiting for the Sun", :artist=> "The Doors"})
-    test_cd.should be_an_instance_of CD
+    expect(test_cd).to be_an_instance_of CD
   end
+  describe 'CD.all' do
+    it 'list all cds in the collection' do
+      CD.clear
+      test_cd = CD.new({:title=> "Waiting for the Sun", :artist=> "The Doors"})
+      test_cd.save
+      test_cd_two = CD.new({:title=> "Swansong", :artist=> "Carcass"})
+      test_cd_two.save
+      test_cd_three = CD.new({:title=> "Abbey Road", :artist=> "The Beatles"})
+      test_cd_three.save
+      expect(CD.all).to eq [test_cd, test_cd_two, test_cd_three]
+    end
+  end
+
+
 end
